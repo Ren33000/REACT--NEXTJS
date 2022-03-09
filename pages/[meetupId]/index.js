4de3f40237075bd834c63fe2fb5a92003522fe7a
Mylene <pont.mylene@gmail.com>
2022-03-09 10:49:14 +0100

    getStaticProps with getStaticPaths added

--- pages/[meetupId]/index.js
+++ pages/[meetupId]/index.js
@@ -11,4 +11,41 @@ function MeetupDetails() {
     )
 }
 
+export async function getStaticPaths() {
+    return {
+        fallback: false ,
+        paths: [
+            { 
+                params: {
+                    meetupId: 'm1',
+                },
+            },
+            {
+                params: {
+                    meetupId: 'm2',
+                },
+            },
+        ]
+    }
+}
+
+export function getStaticProps(context) {
+    // fetch data for a single meetup
+
+    const meetupId = context.params.meetupId;
+    console.log(meetupId);
+
+    return {
+        props: {
+            meetupData: {
+                image: 'https://thumbs.dreamstime.com/z/ikebukuro-shopping-street-tokyo-japan-november-people-shop-attokyo-city-district-night-capital-million-live-its-173860178.jpg',
+                id: meetupId,
+                title: 'First Meetup',
+                address: 'Ikebukuro West gate Park',
+                description: 'A modified version'
+            }
+        }
+    }
+}
+
 export default MeetupDetails;
