082483e17a1c3009769f24b396d0915f931a5cd4
Mylene <pont.mylene@gmail.com>
2022-03-09 10:32:41 +0100

    difference with getServerSideProps

--- pages/index.js
+++ pages/index.js
@@ -19,16 +19,28 @@ function HomePage(props) {
     return <MeetupList meetups={props.meetups} />
 }
 
+// export async function getServerSideProps(context) {
+//     const req = context.req;
+//     const res = context.res;
+
+//     // fetch data from API
+
+//     return {
+//         props: {
+//             meetups: DUMMY_MEETUPS
+//         }
+//     }
+// }
+
 export async function getStaticProps() {
     // fetch data from an API
     return {
         props: {
             meetups: DUMMY_MEETUPS
         },
-        revalidate: 3600
+        revalidate: 1
     };
 }
 
 export default HomePage;
 
-// YT 1:47
